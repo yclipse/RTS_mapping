d3d811e207804d8eeb738871d9a7f980380ec13d
Yili <yyl.eli@gmail.com>
2022-02-10 13:19:13 +0000

    add proj arg to gen roi geometry

--- src/prep/eetool.py
+++ src/prep/eetool.py
@@ -3,13 +3,13 @@ import numpy as np
 import prep.utils
 
 
-def gen_roi_geometry(long, lat):
+def gen_roi_geometry(long, lat, proj='EPSG:4326'):
     '''
     Generate GEE roi geometry polygon using centroid
     '''
     long1, lat1, long2, lat2 = prep.utils.gen_roi(long, lat)
     geometry = ee.Geometry.Polygon(coords=[[[long1, lat1], [long1, lat2], [long2, lat2], [long2, lat1], ]],
-                                   proj='EPSG:4326',
+                                   proj=proj,
                                    geodesic=None,
                                    maxError=1.,
                                    evenOdd=False)
@@ -26,3 +26,6 @@ def band_to_arr(basemap, band: str, roi):
     band_roi = basemap.sampleRectangle(region=roi)
     band_arr = band_roi.get(band)
     return np.array(band_arr.getInfo())
+
+
+def ee_to_tfrecord():
