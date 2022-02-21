17d292159b71e8ecd7fcb943162974ee9bed2fa7
Yili <yyl.eli@gmail.com>
2022-02-21 20:04:48 +0000

    add lenght arg to gen_roi_geometry, add multidirectional_hillshade add shaded_relief

--- src/prep/eetool.py
+++ src/prep/eetool.py
@@ -4,11 +4,11 @@ import prep.utils
 #
 
 
-def gen_roi_geometry(long, lat, proj='EPSG:4326'):
+def gen_roi_geometry(long, lat, lenght=0.01, proj='EPSG:4326'):
     '''
     Generate GEE roi geometry polygon using centroid
     '''
-    long1, lat1, long2, lat2 = prep.utils.gen_roi(long, lat)
+    long1, lat1, long2, lat2 = prep.utils.gen_roi(long, lat, length)
     geometry = ee.Geometry.Polygon(coords=[[[long1, lat1], [long1, lat2], [long2, lat2], [long2, lat1], ]],
                                    proj=proj,
                                    geodesic=None,
@@ -28,3 +28,37 @@ def band_to_arr(basemap, band: str, roi):
     band_roi = basemap.sampleRectangle(region=roi, defaultValue=0)
     band_arr = band_roi.get(band)
     return np.array(band_arr.getInfo())
+
+
+def multidirectional_hillshade(DEM, w_n=0.125, w_ne=0.125, w_e=0.125, w_se=0.125, w_s=0.125, w_sw=0.125, w_w=0.125, w_nw=0.125):
+    '''
+
+    Using eight different traditional hillshades (input,azimuth,altitude).multiply(weight),
+    to produce a weighted multi-directional hillshade.
+
+    '''
+    N = ee.Terrain.hillshade(DEM, 0, 36).multiply(w_n)
+    NE = ee.Terrain.hillshade(DEM, 45, 44).multiply(w_ne)
+    E = ee.Terrain.hillshade(DEM, 90, 56).multiply(w_e)
+    SE = ee.Terrain.hillshade(DEM, 135, 68).multiply(w_se)
+    S = ee.Terrain.hillshade(DEM, 180, 80).multiply(w_s)
+    SW = ee.Terrain.hillshade(DEM, 225, 68).multiply(w_sw)
+    W = ee.Terrain.hillshade(DEM, 270, 56).multiply(w_w)
+    NW = ee.Terrain.hillshade(DEM, 315, 44).multiply(w_nw)
+
+    multi_hillshade = N.add(NE).add(E).add(SE).add(S).add(SW).add(W).add(NW)
+    return multi_hillshade
+
+
+def shaded_relief(slope, hillshade, w_slope=0.5, w_hillshade=0.5):
+    '''
+
+    Mosaic hillshade and slope raster to a single image, which will produce the Shaded Relief, using the ee.ImageCollection.mosaic.
+
+    '''
+    sr = ee.ImageCollection([
+        slope.updateMask(w_slope).rename('b1').toFloat(),
+        hillshade.updateMask(w_hillshade).rename('b1').toFloat()
+    ]).mosaic()
+
+    return sr
