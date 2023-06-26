6b8fdaedc2b5a29f3e2c62f0550a00403078eed8
Yili Yang <97610699+rtsmapping-yili@users.noreply.github.com>
2023-06-26 22:35:39 +0100

    Create inference.py

---
+++ src/models/inference.py
@@ -0,0 +1,157 @@
+from osgeo import gdal,gdal_array
+from prep import dataio, plot, utils
+import numpy as np
+
+
+def readTif(file_name):
+  metadata = gdal.Open(file_name)
+  tif_arr = gdal_array.LoadFile(file_name)
+  width = metadata.RasterXSize
+  height = metadata.RasterYSize
+  bands = metadata.RasterCount
+  geotrans = metadata.GetGeoTransform()
+  proj = metadata.GetProjection()
+  metadata = namedtuple("name", ['width', 'height', 'bands', 'tif_arr', 'geotrans', 'proj'])
+  return metadata(width, height, bands, tif_arr, geotrans, proj)
+
+
+
+def TifCroppingArray(img, SideLength):
+    #  裁剪链表
+    TifArrayReturn = []
+    #  列上图像块数目
+    ColumnNum = int((img.shape[0] - SideLength * 2) / (256 - SideLength * 2))
+    #  行上图像块数目
+    RowNum = int((img.shape[1] - SideLength * 2) / (256 - SideLength * 2))
+    for i in range(ColumnNum):
+        TifArray = []
+        for j in range(RowNum):
+            cropped = img[i * (256 - SideLength * 2) : i * (256 - SideLength * 2) + 256,
+                          j * (256 - SideLength * 2) : j * (256 - SideLength * 2) + 256]
+            TifArray.append(cropped)
+        TifArrayReturn.append(TifArray)
+    #  考虑到行列会有剩余的情况，向前裁剪一行和一列
+    #  向前裁剪最后一列
+    for i in range(ColumnNum):
+        cropped = img[i * (256 - SideLength * 2) : i * (256 - SideLength * 2) + 256,
+                      (img.shape[1] - 256) : img.shape[1]]
+        TifArrayReturn[i].append(cropped)
+    #  向前裁剪最后一行
+    TifArray = []
+    for j in range(RowNum):
+        cropped = img[(img.shape[0] - 256) : img.shape[0],
+                      j * (256-SideLength*2) : j * (256 - SideLength * 2) + 256]
+        TifArray.append(cropped)
+    #  向前裁剪右下角
+    cropped = img[(img.shape[0] - 256) : img.shape[0],
+                  (img.shape[1] - 256) : img.shape[1]]
+    TifArray.append(cropped)
+    TifArrayReturn.append(TifArray)
+    #  列上的剩余数
+    ColumnOver = (img.shape[0] - SideLength * 2) % (256 - SideLength * 2) + SideLength
+    #  行上的剩余数
+    RowOver = (img.shape[1] - SideLength * 2) % (256 - SideLength * 2) + SideLength
+    return TifArrayReturn, RowOver, ColumnOver
+
+
+
+def InfGenerator(TifArray):
+  for i in range(len(TifArray)):
+    for j in range(len(TifArray[0])):
+      img = TifArray[i][j]
+      img = img[...,:-1] # don't have labels
+      img = utils.zScore(img)
+      img = np.expand_dims(img, axis=0)
+      yield img
+
+
+
+#  保存tif文件函数
+def writeTiff(im_data, im_geotrans, im_proj, path):
+    if 'int8' in im_data.dtype.name:
+        datatype = gdal.GDT_Byte
+    elif 'int16' in im_data.dtype.name:
+        datatype = gdal.GDT_UInt16
+    else:
+        datatype = gdal.GDT_Float32
+    if len(im_data.shape) == 3:
+        im_bands, im_height, im_width = im_data.shape
+    elif len(im_data.shape) == 2:
+        im_data = np.array([im_data])
+        im_bands, im_height, im_width = im_data.shape
+
+    #创建文件
+    driver = gdal.GetDriverByName("GTiff")
+    dataset = driver.Create(path, int(im_width), int(im_height), int(im_bands), datatype)
+    if(dataset!= None):
+        dataset.SetGeoTransform(im_geotrans) #写入仿射变换参数
+        dataset.SetProjection(im_proj) #写入投影
+    for i in range(im_bands):
+        dataset.GetRasterBand(i+1).WriteArray(im_data[i])
+    del dataset
+
+
+
+
+#  获得结果矩阵
+def Result(shape, TifArray, npyfile, num_class, RepetitiveLength, RowOver, ColumnOver):
+    result = np.zeros(shape, np.uint8)
+    #  j来标记行数
+    j = 0
+    for i,item in enumerate(npyfile):
+        img = labelVisualize(item)
+        img = img.astype(np.uint8)
+        #  最左侧一列特殊考虑，左边的边缘要拼接进去
+        if(i % len(TifArray[0]) == 0):
+            #  第一行的要再特殊考虑，上边的边缘要考虑进去
+            if(j == 0):
+                result[0 : 256 - RepetitiveLength, 0 : 256-RepetitiveLength] = img[0 : 256 - RepetitiveLength, 0 : 256 - RepetitiveLength]
+            #  最后一行的要再特殊考虑，下边的边缘要考虑进去
+            elif(j == len(TifArray) - 1):
+                #  原来错误的
+                #result[shape[0] - ColumnOver : shape[0], 0 : 256 - RepetitiveLength] = img[0 : ColumnOver, 0 : 256 - RepetitiveLength]
+                #  后来修改的
+                result[shape[0] - ColumnOver - RepetitiveLength: shape[0], 0 : 256 - RepetitiveLength] = img[256 - ColumnOver - RepetitiveLength : 256, 0 : 256 - RepetitiveLength]
+            else:
+                result[j * (256 - 2 * RepetitiveLength) + RepetitiveLength : (j + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength,
+                       0:256-RepetitiveLength] = img[RepetitiveLength : 256 - RepetitiveLength, 0 : 256 - RepetitiveLength]
+        #  最右侧一列特殊考虑，右边的边缘要拼接进去
+        elif(i % len(TifArray[0]) == len(TifArray[0]) - 1):
+            #  第一行的要再特殊考虑，上边的边缘要考虑进去
+            if(j == 0):
+                result[0 : 256 - RepetitiveLength, shape[1] - RowOver: shape[1]] = img[0 : 256 - RepetitiveLength, 256 -  RowOver: 256]
+            #  最后一行的要再特殊考虑，下边的边缘要考虑进去
+            elif(j == len(TifArray) - 1):
+                result[shape[0] - ColumnOver : shape[0], shape[1] - RowOver : shape[1]] = img[256 - ColumnOver : 256, 256 - RowOver : 256]
+            else:
+                result[j * (256 - 2 * RepetitiveLength) + RepetitiveLength : (j + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength,
+                       shape[1] - RowOver : shape[1]] = img[RepetitiveLength : 256 - RepetitiveLength, 256 - RowOver : 256]
+            #  走完每一行的最右侧，行数+1
+            j = j + 1
+        #  不是最左侧也不是最右侧的情况
+        else:
+            #  第一行的要特殊考虑，上边的边缘要考虑进去
+            if(j == 0):
+                result[0 : 256 - RepetitiveLength,
+                       (i - j * len(TifArray[0])) * (256 - 2 * RepetitiveLength) + RepetitiveLength : (i - j * len(TifArray[0]) + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength
+                       ] = img[0 : 256 - RepetitiveLength, RepetitiveLength : 256 - RepetitiveLength]
+            #  最后一行的要特殊考虑，下边的边缘要考虑进去
+            if(j == len(TifArray) - 1):
+                result[shape[0] - ColumnOver : shape[0],
+                       (i - j * len(TifArray[0])) * (256 - 2 * RepetitiveLength) + RepetitiveLength : (i - j * len(TifArray[0]) + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength
+                       ] = img[256 - ColumnOver : 256, RepetitiveLength : 256 - RepetitiveLength]
+            else:
+                result[j * (256 - 2 * RepetitiveLength) + RepetitiveLength : (j + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength,
+                       (i - j * len(TifArray[0])) * (256 - 2 * RepetitiveLength) + RepetitiveLength : (i - j * len(TifArray[0]) + 1) * (256 - 2 * RepetitiveLength) + RepetitiveLength,
+                       ] = img[RepetitiveLength : 256 - RepetitiveLength, RepetitiveLength : 256 - RepetitiveLength]
+    return result
+
+
+
+def labelVisualize(img):
+    img_out = np.zeros((img.shape[0],img.shape[1]))
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            #  为第n类赋上n值
+            img_out[i][j] = np.argmax(img[i][j])
+    return img_out
